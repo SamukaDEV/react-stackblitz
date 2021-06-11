f96042f3311081aa78ec2dbe2d779bf7ad7a617b
SamukaDEV <absfregs@gmail.com>
2021-06-11 08:40:23 -0300

    3 files modified

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import './style.css';
 
 export default function App() {
   return (
-    <div>
+    <div className="container">
       <h1>Hello Smk!</h1>
       <p>SamukaDEV was here</p>
+      <div className="card">
+        <div className="card-body">Card</div>
+      </div>
     </div>
   );
 }
