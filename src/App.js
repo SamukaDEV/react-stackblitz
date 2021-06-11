c7ba937e5c556ab24725579d2075c2de80da2f00
SamukaDEV <absfregs@gmail.com>
2021-06-11 08:36:26 -0300

    1 file modified

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div>
       <h1>Hello Smk!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <p>SamukaDEV was here</p>
     </div>
   );
 }
