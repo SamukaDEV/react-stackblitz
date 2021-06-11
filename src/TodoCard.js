b62a5d326d125e86f78c21a6d7b61fc02a34e494
SamukaDEV <absfregs@gmail.com>
2021-06-11 11:03:44 -0300

    1 file modified

--- src/TodoCard.js
+++ src/TodoCard.js
@@ -1,7 +1,7 @@
 import React from 'react';
 export default function TodoCard(props) {
   return (
-    <div className="card mb-2" key={props.key}>
+    <div className="card mb-2">
       <div className="card-body">
         <i
           className="bi bi-trash text-danger remove-button"
